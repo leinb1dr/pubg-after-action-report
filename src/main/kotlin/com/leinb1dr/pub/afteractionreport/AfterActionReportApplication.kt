1c6d7c29788058c3a0491bb82392d4e22265c9b9
Dan Leinbach <leinb1dr@gmail.com>
2022-11-02 22:42:28 -0400

    Fixed metrics by adding a low batch size.

--- src/main/kotlin/com/leinb1dr/pub/afteractionreport/AfterActionReportApplication.kt
+++ src/main/kotlin/com/leinb1dr/pub/afteractionreport/AfterActionReportApplication.kt
@@ -1,29 +1,11 @@
 package com.leinb1dr.pub.afteractionreport
 
-import io.micrometer.core.instrument.MeterRegistry
-import io.micrometer.stackdriver.StackdriverConfig
-import io.micrometer.stackdriver.StackdriverMeterRegistry
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
 
 
 @SpringBootApplication
-class AfterActionReportApplication{
-    @Bean
-    fun meterRegistry(): MeterRegistry? {
-        val stackdriverConfig: StackdriverConfig = object : StackdriverConfig {
-            override fun get(key: String): String? {
-                return null
-            }
-
-            override fun projectId(): String {
-                return "leinb1dr-test-application-1"
-            }
-        }
-        return StackdriverMeterRegistry.builder(stackdriverConfig).build()
-    }
-}
+class AfterActionReportApplication
 
 fun main(args: Array<String>) {
     runApplication<AfterActionReportApplication>(*args)
