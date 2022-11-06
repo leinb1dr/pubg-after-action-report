10f8fb95d454336d80d7e37720ed55108559be7d
Dan Leinbach <leinb1dr@gmail.com>
2022-11-05 20:41:52 -0400

    checking something

--- src/test/kotlin/com/leinb1dr/pub/afteractionreport/AfterActionReportApplicationTests.kt
+++ src/test/kotlin/com/leinb1dr/pub/afteractionreport/AfterActionReportApplicationTests.kt
@@ -1,6 +1,9 @@
 package com.leinb1dr.pub.afteractionreport
 
+import com.leinb1dr.pub.afteractionreport.config.AppProperties
 import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Import
 import org.springframework.test.context.ActiveProfiles
@@ -8,11 +11,14 @@ import org.springframework.test.context.ActiveProfiles
 @SpringBootTest
 @ActiveProfiles("test")
 @Import(com.leinb1dr.pub.afteractionreport.config.TestConfiguration::class)
-class AfterActionReportApplicationTests {
+class AfterActionReportApplicationTests(@Autowired val appProperties: AppProperties) {
+
+	private val logger = LoggerFactory.getLogger(AfterActionReportApplicationTests::class.java)
 
 	@Test
 	fun contextLoads() {
-
+		logger.info("CHECKHERE " + appProperties.pubgToken)
+		logger.info("CHECKHERE " + appProperties.discordWebHook)
 	}
 
 }
